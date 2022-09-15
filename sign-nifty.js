c4e22d4961a5aac55193a2f08d8f004b3393e0e5
Shahaf Antwarg <santwarg@gmail.com>
2022-09-15 15:10:36 +0300

    First Commit

--- sign-nifty.js
+++ sign-nifty.js
@@ -3,25 +3,6 @@ const {
   fromRpcSig, bufferToHex, toBuffer,
 } = require('ethereumjs-util');
 
-const EIP712Domain = [
-  {
-    name: 'name',
-    type: 'string',
-  },
-  {
-    name: 'version',
-    type: 'string',
-  },
-  {
-    name: 'chainId',
-    type: 'uint256',
-  },
-  {
-    name: 'verifyingContract',
-    type: 'address',
-  },
-];
-
 const Order = [
   {
     name: 'makerAddress',
@@ -68,36 +49,6 @@ const Order = [
     type: 'bytes',
   },
 ];
-
-/**
- *   @send - send message to and open metamask
- */
-const send = (provider, data) => new Promise((resolve, reject) => provider.send(data, (err, result) => {
-  if (result.error) {
-    err = result.error;
-  }
-  if (err) {
-    // console.log(err, result);
-    return reject(err);
-  }
-  // console.log(result);
-  return resolve(result.result);
-}));
-/**
- *   @signTypedData - function that handles signing and metamask interaction
- */
-const signTypedData = async (provider, address, payload) => {
-  const typedData = {
-    id    : '44',
-    params: [
-      address,
-      payload,
-    ],
-    jsonrpc: '2.0',
-    method : 'eth_signTypedData_v4',
-  };
-  return send(provider, typedData);
-};
 
 /**
  *   @signTyped - main function to be called when signing
